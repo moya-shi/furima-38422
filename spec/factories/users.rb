934ddfb268a4e1d2b9a6b772a272b92a137c9744
moya-shi <keisu.6739mo@gmail.com>
2023-01-02 23:55:39 +0900

    ユーザー新規登録テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
 
     japanese_user = Gimei.name
 
-    nickname              {Faker::Japanese::Name.name}
+    nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
@@ -11,5 +11,6 @@ FactoryBot.define do
     first_name            {japanese_user.first.kanji}
     last_name_kana        {japanese_user.last.katakana}
     first_name_kana       {japanese_user.first.katakana}
+    birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end
