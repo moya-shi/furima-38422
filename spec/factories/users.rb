1a584f0774215ad89647a8f71f119b922918373c
moya-shi <keisu.6739mo@gmail.com>
2023-01-02 13:53:42 +0900

    Gimeiの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,15 @@
 FactoryBot.define do
   factory :user do
+
+    japanese_user = Gimei.name
+
     nickname              {Faker::Japanese::Name.name}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name             {Faker::Japanese::Name.last_name}
-    first_name            {Faker::Japanese::Name.first_name}
-    last_name_kana        {Faker::}
-    first_name_kana
+    last_name             {japanese_user.last.kanji}
+    first_name            {japanese_user.first.kanji}
+    last_name_kana        {japanese_user.last.katakana}
+    first_name_kana       {japanese_user.first.katakana}
   end
 end
