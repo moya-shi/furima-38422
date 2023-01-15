0c41b51fd58481fc504aa4ca8921c0fdecf4356f
moya-shi <keisu.6739mo@gmail.com>
2023-01-15 10:41:15 +0900

    ActiveHashモデルクラスの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :shipping_cost, :shipping_address, :shipping_day
+
+  validates :name, :explain, :price, presence: true
+  validates :category_id, :condition_id, :shipping_cost_id, :shipping_address_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
