b7c4e0ea520530f6cb2807885d991f6ed7f76e09
moya-shi <keisu.6739mo@gmail.com>
2023-01-15 12:14:22 +0900

    バリデーションの設定と商品出品ページ遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,9 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
   # def item_params
   #   params.require(:item).permit(:name, :explain, :category_id, :condition_id, :shipping_cost_id, :shipping_address_id, :shipping_day_id, :price, :user_id)
   # end
